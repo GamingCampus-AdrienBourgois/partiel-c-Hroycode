f33df89031bab272d7006e0181dbfab69df4f9fa
Hroycode <hroy@gaming.tech>
2024-01-19 14:25:11 +0100

    debut exo 3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,15 +1,37 @@
 #include "Solution3.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <cctype>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_3
+
+std::string toLowerCase(const std::string& str) {
+    std::string lowerCaseStr = str;
+    std::transform(lowerCaseStr.begin(), lowerCaseStr.end(), lowerCaseStr.begin(),
+        [](unsigned char c) { return std::tolower(c); });
+    return lowerCaseStr;
+}
 
 void Solution3::SetWords(const std::vector<std::string>& _words)
 {
 	words = _words;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() {
+    if (words.empty()) {
+        throw std::runtime_error("Vector is empty");
+    }
+
+    std::sort(words.begin(), words.end(),
+        [](const std::string& a, const std::string& b) {
+            return toLowerCase(a) < toLowerCase(b);
+        });
+}
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
